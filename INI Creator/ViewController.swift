76890c3b6de0e105d2cefd08aac5d91a72e112c5
aelarassi <abdelaziz.elarassi@sii-maroc.com>
2023-06-03 01:41:13 +0100

    Initial Commit

---
+++ INI Creator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INI Creator
+//
+//  Created by aelarassi on 3/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
